605a65e8ca07dc94b8e57695af87f6d5e244c888
kraaaam <164256192+kraam0@users.noreply.github.com>
2025-03-19 19:16:11 +0100

    add the new lines at the text.py

--- text.py
+++ text.py
@@ -1,5 +1,18 @@
-print("nothing")
-print("at")
-print("nothin'")
+# addition == +
+# subtraction == -
+# multiplication == *
+# division == /
+operator = input("entre an operator + - / * : ")
+number1 = float(input("entre the first number : "))
+number2 = float(input("entre the second number : "))
 
-
+if operator == "+":
+    print(number1 + number2)
+elif operator == "-":
+    print(number1 - number2)
+elif operator == "*":
+    print(number1 * number2)
+elif operator == "/":
+    print(number1 / number2)
+else:
+    print("you didnt entre an operator")  
